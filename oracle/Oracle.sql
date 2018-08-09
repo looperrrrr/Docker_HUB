71b3a90f8e9593450b55b7b3980ba4d383e9219c
looeprf <valentinaaba2@gmail.com>
2018-08-09 17:35:12 +0300

    Script for creating table and data

---
+++ oracle/Oracle.sql
@@ -0,0 +1,21 @@
+-- user/password=Scott/123
+begin
+CREATE TABLE Car(
+	car_id INTEGER NOT NULL,
+	cname VARCHAR(60) NOT NULL,
+	color VARCHAR(60),
+	speed INTEGER,
+	Manufactured_Country VARCHAR(100),
+	PRIMARY KEY(car_id));
+    
+    
+INSERT INTO Car VALUES(1,'Zen','vova11',45.34,'India');
+INSERT INTO Car VALUES(2,'Volkswagen','Black',49.64,'Germany');
+INSERT INTO Car VALUES(3,'Polo','White',52.33,'Japan');
+INSERT INTO Car VALUES(4,'Audi','Blue',55.98,'Germany');
+INSERT INTO Car VALUES(5,'Innova','Maroon',39.97,'France');
+INSERT INTO Car VALUES(6,'FiatPalio','Silver',35.45,'Italy');
+INSERT INTO Car VALUES(7,'Qualis','Red',23.35,'Paris');
+
+commit;
+end;
